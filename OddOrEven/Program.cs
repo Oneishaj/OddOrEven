f825d72d27482fc621263a1dab1e780ccb7ab1ff
Oneisha Jones <oneishaj79@gmail.com>
2020-10-27 12:07:45 -0500

    Completed

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -6,7 +6,34 @@ namespace OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(IsOdd(5));
         }
+
+        public static bool IsOdd(int num) 
+        {
+            
+
+            if(num % 2 == 0) 
+            {
+                Console.WriteLine("This is Even");
+                return false;
+            }
+            else 
+            {
+                Console.WriteLine("This is odd");
+                return true;
+            }
+        }
+         
+
+        
+
+
+        
+
+             
+         
+
+
     }
 }
